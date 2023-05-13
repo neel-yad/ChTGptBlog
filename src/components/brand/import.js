aa7fecb3896ae27a68253845e04cb37730e51127
Neeraj Yadav <neerjyadav21@gmail.com>
2023-05-14 01:43:31 +0530

    first Commit

---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.svg';
+import slack from '../../assets/instagram.svg';
+import atlassian from '../../assets/apple.svg';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/facebook.svg';
+
+export {
+  google,
+  slack,
+  atlassian,
+  dropbox,
+  shopify,
+};
---
+++ src/containers/blog/import.js
@@ -0,0 +1,13 @@
+import blog01 from '../../assets/people01.png';
+import blog02 from '../../assets/people01.png';
+import blog03 from '../../assets/people01.png';
+import blog04 from '../../assets/people03.png';
+import blog05 from '../../assets/people03.png';
+
+export {
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+};
